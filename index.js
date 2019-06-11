ea59dfebf057dc4d133f84d194f3e124f071a408
brennanmrose <brennan.m.rose@gmail.com>
2019-06-10 20:55:09 -0400

    Done.

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-	return document.querySelector('#grand-node div div div div')
+  let node = document.getElementById( 'grand-node' )
+  let nextNode = node.children[ 0 ]
+
+  while ( nextNode ) {
+    node = nextNode
+    nextNode = node.children[ 0 ]
+  }
+
+  return node
 }
 
 function increaseRankBy( n ) {
